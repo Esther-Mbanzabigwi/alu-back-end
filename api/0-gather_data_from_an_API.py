92aba3a444a1f4256e61b4160553ef0d2efaf973
Esther-Mbanzabigwi <e.mbanzabigwi@alustudent.com>
2023-07-09 21:16:38 +0000

    changed

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     todos = json.loads(res.text)
     done = []
     for i in todos:
-        if i['ok']:
+        if i['completed']:
             done.append(i)
     print("Employee {} is done with tasks({}/{}):".format(
                                                           user['name'],
